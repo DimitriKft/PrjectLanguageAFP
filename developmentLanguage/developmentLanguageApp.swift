307f9b8087478d31909b4f0e93300cdd6d550abf
Dimitri Klopfstein <dimitri.kft@gmail.com>
2022-09-28 15:15:47 +0200

    one

---
+++ developmentLanguage/developmentLanguageApp.swift
@@ -0,0 +1,17 @@
+//
+//  developmentLanguageApp.swift
+//  developmentLanguage
+//
+//  Created by dimitri on 28/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct developmentLanguageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
